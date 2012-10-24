bd459697381205ad45a5ebc06a6e2251c738b984
Daniel P. Rossi <DR9885@gmail.com>
2012-10-23 20:29:05 -0400

    Merge

---
+++ Development/Pong.Box2D/Pong.Box2D/Input.cpp
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+#include "Input.h"
+
+Input* Input::instance = 0;
---
+++ Development/Pong.Box2D/Pong.Box2D/Input.h
@@ -0,0 +1,71 @@
+
+#pragma once
+
+#ifndef INPUT_H
+#define INPUT_H
+/* Singleton */ 
+
+enum KeyCode {
+	NONE,
+	LEFT	= GLUT_KEY_LEFT, 
+	RIGHT	= GLUT_KEY_RIGHT,
+	UP		= GLUT_KEY_UP,
+	DOWN	= GLUT_KEY_DOWN
+};
+
+class Input {
+public: 
+	static Input* Instance() {
+		/* Lazy Instantiation */
+		if(instance == 0)
+			instance = new Input();
+		return instance;
+	}
+
+	static bool GetKey(char c) {
+		return false;
+	}
+
+	static bool GetKey(KeyCode keyCode) {
+		
+
+		return false;
+	}
+
+
+
+private:
+	map<int, int> *keys;
+
+	static Input* instance;
+
+	Input& operator=(Input const&){}; // Assignment Operator 
+	Input(Input const&){}; // Copy Constructor
+	Input() {
+		keys = new map<int, int>();
+		
+		
+		//glutSpecialFunc(KeyDownSpecial);
+
+		//glutSpecialUpFunc(KeyUpSpecial);
+	}
+
+	
+	static void KeyDown(unsigned char key, int x, int y) {
+		cout<<"down";
+	}
+
+	static void KeyUp(unsigned key, int x, int y) {
+
+	}
+
+	static void KeyDownSpecial(int key, int x, int y) {
+		cout<<"down";
+	}
+
+	static void KeyUpSpecial(int key, int x, int y) {
+
+	}
+};
+
+#endif
